c6d9acbbfac0272f80a6812cbae2464fc89253c6
myml <wurongjie@deepin.org>
2022-11-21 17:18:53 +0800

    feat: 对文件类型也进行检查

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"runtime"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/88250/lute"
@@ -17,13 +20,20 @@ import (
 var (
 	// ErrNotFoundLastModified 响应请求没有LastModified头
 	ErrNotFoundLastModified = errors.New("not found last modified")
+	// ErrUnknownContentType 未知的文件类型
+	ErrUnknownContentType = errors.New("unknown content type")
 )
+
+var HeadContentType = "application/octet-stream"
+var Timeout = time.Second * 10
 
 func main() {
 	var packagePATH, releasePATH, checkFile string
 	flag.StringVar(&packagePATH, "packages_path", "", "package mirrors markdown file path")
 	flag.StringVar(&releasePATH, "releases_path", "", "release mirrors markdown file path")
 	flag.StringVar(&checkFile, "check_file", "", "check file path")
+	flag.StringVar(&HeadContentType, "content_type", HeadContentType, "response content type")
+	flag.DurationVar(&Timeout, "timeout", Timeout, "request timeout")
 	flag.Parse()
 	switch {
 	case len(packagePATH) > 0:
@@ -50,50 +60,48 @@ func checkPackage(path, checkFile string) {
 	if err != nil {
 		panic(err)
 	}
-	// var officialContentLength int64
-	// var officialLastModified *time.Time
-	// doc.Find("a").Each(func(i int, s *goquery.Selection) {
-	// 	href := s.AttrOr("href", "")
-	// 	if !strings.Contains(href, "deepin.com") {
-	// 		return
-	// 	}
-	// 	if officialContentLength == 0 {
-	// 		officialContentLength, officialLastModified, err = head(inReleaseURL(href))
-	// 		if err != nil {
-	// 			panic(err)
-	// 		}
-	// 	}
-	// })
+	hrefList := make(map[string]struct{})
 	doc.Find("table").First().Find("a").Each(func(_ int, s *goquery.Selection) {
 		href := s.AttrOr("href", "")
-		if !strings.HasPrefix(href, "http") {
-			// 标记不支持的协议
-			// fmt.Printf("/* url: %s msg: Unsupported protocol */\n", href)
-			// fmt.Println(cssA(href, "black", "lightgrey"))
-			return
-		}
-		contentLength, lastModified, err := head(strings.Trim(href, "/") + checkFile)
-		if err != nil {
-			log.Println(href, "失败", err)
+		hrefList[href] = struct{}{}
+	})
+
+	// 生成css
+	errChan := make(chan [2]string)
+	defer close(errChan)
+	go func() {
+		for v := range errChan {
+			href, err := v[0], v[1]
 			fmt.Printf("/* url: %s msg: %s */\n", href, err)
 			fmt.Println(cssA(href, "white", "red"))
 			fmt.Println()
-			return
 		}
-		log.Println(href, "有效")
-		_ = contentLength
-		_ = lastModified
-		// 标记过期的镜像仓库
-		// if contentLength != officialContentLength {
-		// 	if officialLastModified.Sub(*lastModified) > time.Hour*24*7 {
-		// 		log.Println(href, "过时")
-		// 		fmt.Printf("/* url: %s msg: Outdated */\n", href)
-		// 		fmt.Println(cssA(href, "black", "yellow"))
-		// 		fmt.Println()
-		// 		return
-		// 	}
-		// }
-	})
+	}()
+	// 检查
+	var limitChan = make(chan struct{}, runtime.NumCPU())
+	var wg sync.WaitGroup
+	for href := range hrefList {
+		limitChan <- struct{}{}
+		wg.Add(1)
+		go func(href string) {
+			defer func() {
+				wg.Done()
+				<-limitChan
+			}()
+			if !strings.HasPrefix(href, "http") {
+				log.Println(href, "不支持")
+				return
+			}
+			_, _, err := head(strings.Trim(href, "/") + checkFile)
+			if err != nil {
+				log.Println(href, "失败", err)
+				errChan <- [2]string{href, err.Error()}
+				return
+			}
+			log.Println(href, "有效")
+		}(href)
+	}
+	wg.Wait()
 }
 
 // 检查ISO镜像源
@@ -102,27 +110,49 @@ func checkRelease(path, checkFile string) {
 	if err != nil {
 		panic(err)
 	}
+	hrefList := make(map[string]struct{})
 	doc.Find("table").First().Find("a").Each(func(_ int, s *goquery.Selection) {
 		href := s.AttrOr("href", "")
-		if !strings.HasPrefix(href, "http") {
-			// 标记不支持的协议
-			// fmt.Printf("/* url: %s msg: Unsupported protocol */\n", href)
-			// fmt.Println(cssA(href, "black", "lightgrey"))
-			return
-		}
-		_, _, err := head(strings.Trim(href, "/") + checkFile)
-		if err != nil {
-			if errors.Is(err, ErrNotFoundLastModified) {
-				return
-			}
-			log.Println(href, "失败", err)
+		hrefList[href] = struct{}{}
+	})
+
+	// 生成css
+	errChan := make(chan [2]string)
+	defer close(errChan)
+	go func() {
+		for v := range errChan {
+			href, err := v[0], v[1]
 			fmt.Printf("/* url: %s msg: %s */\n", href, err)
 			fmt.Println(cssA(href, "white", "red"))
 			fmt.Println()
-			return
 		}
-		log.Println(href, "有效")
-	})
+	}()
+
+	// 检查
+	var limitChan = make(chan struct{}, runtime.NumCPU())
+	var wg sync.WaitGroup
+
+	for href := range hrefList {
+		limitChan <- struct{}{}
+		wg.Add(1)
+		go func(href string) {
+			defer func() {
+				wg.Done()
+				<-limitChan
+			}()
+			if !strings.HasPrefix(href, "http") {
+				log.Println(href, "不支持")
+				return
+			}
+			_, _, err := head(strings.Trim(href, "/") + checkFile)
+			if err != nil {
+				log.Println(href, "失败", err)
+				errChan <- [2]string{href, err.Error()}
+				return
+			}
+			log.Println(href, "有效")
+		}(href)
+	}
 }
 
 // 发送HEAD请求
@@ -131,6 +161,9 @@ func head(url string) (ContentLength int64, LastModified *time.Time, err error) 
 	if err != nil {
 		return 0, nil, err
 	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	req = req.WithContext(ctx)
 	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -143,6 +176,10 @@ func head(url string) (ContentLength int64, LastModified *time.Time, err error) 
 	contentLength := resp.ContentLength
 	lastModifiedStr := resp.Header.Get("Last-Modified")
 	if len(lastModifiedStr) == 0 {
+		return 0, nil, ErrNotFoundLastModified
+	}
+	contentType := resp.Header.Get("Content-Type")
+	if contentType != "application/octet-stream" {
 		return 0, nil, ErrNotFoundLastModified
 	}
 	t, err := time.Parse(http.TimeFormat, lastModifiedStr)
