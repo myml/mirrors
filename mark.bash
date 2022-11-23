1fa53251dbc253c0e7e2731d32df39511199237d
myml <wurongjie@deepin.org>
2022-11-23 11:53:40 +0800

    feat: mirror check

--- mark.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-for type in packages releases;do
-    for lang in zh en;do
-        go run github.com/myml/mirrors -${type}_url https://www.deepin.org/developer-center/mirrors/${type}_${lang}.html > ${type}_${lang}_mark.css &
-    done
-done
-wait
